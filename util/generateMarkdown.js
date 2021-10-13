5991551a5f31822c99365e9f2dddf1b8764b7492
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-10-13 00:53:18 -0500

    This commit adds the bulk of the page layout for the readme generator, it also places the layout onto an actual readme when run

--- util/generateMarkdown.js
+++ util/generateMarkdown.js
@@ -1,48 +1,83 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
+generateMarkdown(license);
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
+// function renderLicenseLink(license) {
 
-}
+// }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {
 
-}
+// }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
 
-  return `# ${data.title}
-
-  ##Description
-
-  ${data.description}
-  
-  ##Installation
-
-  ${data.description}
-
-  ##Usage
-
-  ${data.description}
-
-  ##Contribution Guidelines
-
-  ${data.description}
-
-  ##Tests
-
-  ${data.description}
-
-
+if (data.license){
+  var x = `## Licence
+- - - - 
+insert license info through if or case statements 
 `;
+// if(data.npm){
+//   var npm = `npm i ${data.installation}`
+// }
+  // ${[license](licenselink)}
 }
 
+  return `# ${data.title}
+badge placeholder for: ${data.license}
+
+## Table of Contents
+ - - - -
+* [Description](#Description "Goto Description")
+* [Installation](#Installation "Goto Installation")
+* [Usage](#Usage "Goto Usage")
+* [Contributions](#Contributions "Goto Contributions")
+* [Tests](#Tests "Goto Tests")
+* [Questions](#Questions "Goto Questions")
+
+## Description
+ - - - -
+${data.description}
+  
+## Installation
+ - - - -
+${data.installation}
+
+${npm}
+
+## Usage
+ - - - -
+${data.usage}
+
+[Application in action](${data.usagevideo})
+
+## Contribution Guidelines
+ - - - -
+${data.contribution_guidelines}
+
+## Tests
+ - - - -
+${data.tests}
+
+${x}
+
+## Questions
+ - - - -
+To see more of my projects visit my page below:
+[My GitHub](https://github.com/${data.username})
+
+If you have any questions you can contact me through my email:
+${data.email}
+- - - -
+[Back to Top](#${data.title} "Goto top")
+`;
+}
+module.exports = renderLicenseBadge;
 module.exports = generateMarkdown;
--- util/index.js
+++ util/index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
 const inquirer = require('inquirer');
 const generateMarkdown = require('./generateMarkdown');
-var markdown = require('./generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -12,13 +12,13 @@ const questions = [
     },
     {
         type:'input',
-        name:'Description',
+        name:'description',
         message:'Give a short description explaining the what, why, and how of this project.',
         default:'Test'
     },
     {
         type:'input',
-        name:'Installation',
+        name:'installation',
         message:'Give a step-by-step instructional guide on what needs to be installed in order for you application to run.',
         default:'fork repo and run'
     },
@@ -30,24 +30,44 @@ const questions = [
     },
     {
         type:'input',
-        name:'contribution guidelines',
+        name:'contribution_guidelines',
         message:'List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well.',
         default:'New Project'
     },
     {
         type:'input',
-        name:'test instructions',
+        name:'tests',
         message:'If you have any tests for your application, provide an example on how use them here.',
         default:'No tests'
     },
-]
+    {
+        type:'list',
+        name:'license',
+        choices: ['MIT','GNU GPLv3','Apache','Mozilla','ISC'],
+    },
+    {
+        type:'input',
+        name:'username',
+        message:'please enter your github username',
+        default:''
+    },
+    {
+        type:'input',
+        name:'email',
+        message:'please enter your email adress',
+        default:''
+    }
+];
 inquirer.prompt(questions)
-.then((answers)=>{
+.then((answers)=>{ 
     console.log(generateMarkdown(answers));
+    fs.writeFile("README.md", generateMarkdown(answers), (err) =>{
+    err ? console.log(err) : console.log("success");
+    }) ;
 })
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
 function init() {}
@@ -59,13 +79,9 @@ init();
 // WHEN I am prompted for information about my application repository
 // THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
 // WHEN I choose a license for my application from a list of options
 // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
+
 // WHEN I click on the links in the Table of Contents
 // THEN I am taken to the corresponding section of the README
